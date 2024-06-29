c15bccdb042d19dcec5ddb43c01613820a82af23
Favillion56 <dannyralph_1@hotmailcom>
2024-06-29 20:25:41 +0100

    base code

---
+++ index.js
@@ -0,0 +1,30 @@
+    console.log("Maths fizzbang test")
+
+    let number1 = 2
+
+    let number2 = 3
+
+
+    console.log(number1)
+
+
+
+    let div = number1 / number2
+
+    
+
+
+    //condtional operator
+
+     let back1 = 2
+
+
+
+     function calculatefizzbang() {
+
+
+
+     }
+
+     //fizzbang console log operator
+     console.log( ---------- ) 
