0dddc971c0768a947a4da7758994186fa868d4bd
Favillion56 <dannyralph_1@hotmailcom>
2024-07-01 13:05:39 +0100

    updateone

--- index.js
+++ index.js
@@ -7,24 +7,12 @@
 
     console.log(number1)
 
-
-
     let div = number1 / number2
 
-    
+    const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"];
 
-
-    //condtional operator
-
-     let back1 = 2
-
-
-
-     function calculatefizzbang() {
-
-
-
-     }
-
-     //fizzbang console log operator
-     console.log( ---------- ) 
+let text = "";
+for (let info = 0; info < numbers.length; info++) {
+  text += numbers[info] + "<br>";
+}
+// somehow make const numbers, be tracked by a false or true statement that recgnises those div by 3 or 5 to make fixxbang
